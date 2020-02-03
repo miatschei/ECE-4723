003948862093cec16940e434232f22d616da9bb0
miatschei <33326966+miatschei@users.noreply.github.com>
2020-02-02 18:42:16 -0600

    changed RF5 to RF4 bc I cant read

--- revF14.h
+++ revF14.h
@@ -6,16 +6,16 @@
 #include "pic24_all.h"
 
 // Lab 2 Definitions and Macros
-#define CONFIG_LED1 ()    CONFIG_RF5_AS_DIG_OUTPUT()
-#define LED1              (_LATF5)
+#define CONFIG_LED1()     CONFIG_RF4_AS_DIG_OUTPUT()
+#define LED1              (_LATF4)
 
-#define CONFIG_LED2 ()    CONFIG_RB14_AS_DIG_OUTPUT()
+#define CONFIG_LED2()     CONFIG_RB14_AS_DIG_OUTPUT()
 #define LED2              (_LATB14) 
 
-#define CONFIG_LED3 ()    CONFIG_RB15_AS_DIG_OUTPUT()
+#define CONFIG_LED3()     CONFIG_RB15_AS_DIG_OUTPUT()
 #define LED3              (_LATB15) 
 
-#define CONFIG_SW1 ()     {\
+#define CONFIG_SW1()       {\
                             CONFIG_RB13_AS_DIG_INPUT();\
                             ENABLE_RB13_PULLUP();\
                             DELAY_US(1);\
@@ -25,7 +25,7 @@
 #define SW1_PRESSED()      (_RB13 == 0)
 #define SW1_RELEASED()     (_RB13 == 1)
 
-#define CONFIG_SW2 ()      {\
+#define CONFIG_SW2()       {\
                             CONFIG_RB12_AS_DIG_INPUT();\
                             ENABLE_RB12_PULLUP();\
                             DELAY_US(1);\
@@ -35,7 +35,7 @@
 #define SW2_PRESSED()      (_RB12 == 0)
 #define SW2_RELEASED()     (_RB12 == 1)
 
-#define CONFIG_SW3 ()      {\
+#define CONFIG_SW3()       {\
                             CONFIG_RC15_AS_DIG_INPUT();\
                             ENABLE_RC15_PULLUP();\
                             DELAY_US(1);\
