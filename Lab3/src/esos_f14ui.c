5948aca51636d8d351671caca870ad43c7b99620
Jonahz <jonahgandy@gmail.com>
2020-02-17 21:16:51 -0600

    led double press functionality

--- Lab3/src/esos_f14ui.c
+++ Lab3/src/esos_f14ui.c
@@ -176,21 +176,146 @@ ESOS_USER_TIMER(debug_rpg){
 }
 
 
+uint32_t sw1_old_time = 0; 
+uint16_t sw1_state = 0;
+uint16_t sw1_flash_counter = 0;
 
+//     SW1_IDLE 0
+//     SW1_SINGLE_HIGH  1
+//     SW1_SINGLE_LOW 2
+//     SW1_WAIT  3
+
+ESOS_USER_TASK(switch_t1){
+  ESOS_TASK_BEGIN();
+  while(true){
+    if(SW1_PRESSED && sw1_state == 0){
+      ESOS_TASK_WAIT_TICKS( 25 ); //debounce
+      _st_esos_uiF14Data.b_SW1Pressed = true; 
+      LED1 = 1;
+      sw1_state = 1; 
+      __esos_unsafe_PutString("state: 1");
+    }
+    else if (sw1_state == 1){
+      //__esos_unsafe_PutString("state: 1");
+      _st_esos_uiF14Data.b_SW1Pressed = false; 
+      LED1 = 1; 
+      if (SW1_RELEASED){
+        ESOS_TASK_WAIT_TICKS( 25 ); //debounce
+        LED1 = 0;
+        sw1_state = 2;
+        __esos_unsafe_PutString("state: 2");
+        sw1_old_time = esos_GetSystemTick();
+      }
+    }
+    else if (sw1_state == 2){
+      //wait for second press here
+      if ((esos_GetSystemTick() - sw1_old_time) >= 1500){
+        __esos_unsafe_PutString("timer expired :(, returning to state 0");
+        sw1_state = 0; //go back to first state
+      }
+
+      if (((esos_GetSystemTick() - sw1_old_time) <= 1500) && SW1_PRESSED){
+        ESOS_TASK_WAIT_TICKS( 25 ); //debounce
+        __esos_unsafe_PutString("double press bby! flash time");
+        sw1_state = 3;
+      }
+    }
+    else if(sw1_state == 3){
+      //blink led
+      while (sw1_flash_counter < 6){
+        LED1_TOGGLE();
+        ESOS_TASK_WAIT_TICKS( 250 );
+        sw1_flash_counter++;
+      }
+      sw1_flash_counter = 0;
+      LED1 = 0;
+      //return to first state
+      sw1_state = 0;
+
+    }
+    ESOS_TASK_YIELD();
+  }
+  ESOS_TASK_END();
+}
+
+uint32_t sw2_old_time = 0; 
+uint16_t sw2_state = 0;
+uint16_t sw2_flash_counter = 0;
+
+ESOS_USER_TASK(switch_t2){
+  ESOS_TASK_BEGIN();
+  while(true){
+    if(SW2_PRESSED && sw2_state == 0){
+      ESOS_TASK_WAIT_TICKS( 25 ); //debounce
+      _st_esos_uiF14Data.b_SW1Pressed = true; 
+      LED1 = 1;
+      sw2_state = 1; 
+      __esos_unsafe_PutString("state: 1");
+    }
+    else if (sw2_state == 1){
+      //__esos_unsafe_PutString("state: 1");
+      _st_esos_uiF14Data.b_SW1Pressed = false; 
+      LED1 = 1; 
+      if (SW2_RELEASED){
+        ESOS_TASK_WAIT_TICKS( 25 ); //debounce
+        LED1 = 0;
+        sw2_state = 2;
+        __esos_unsafe_PutString("state: 2");
+        sw2_old_time = esos_GetSystemTick();
+      }
+    }
+    else if (sw2_state == 2){
+      //wait for second press here
+      if ((esos_GetSystemTick() - sw2_old_time) >= 1500){
+        __esos_unsafe_PutString("timer expired :(, returning to state 0");
+        sw2_state = 0; //go back to first state
+      }
+
+      if (((esos_GetSystemTick() - sw2_old_time) <= 1500) && SW2_PRESSED){
+        ESOS_TASK_WAIT_TICKS( 25 ); //debounce
+        __esos_unsafe_PutString("double press bby! flash time");
+        sw2_state = 3;
+      }
+    }
+    else if(sw2_state == 3){
+      //blink led
+      while (sw2_flash_counter < 6){
+        LED1_TOGGLE();
+        ESOS_TASK_WAIT_TICKS( 250 );
+        sw2_flash_counter++;
+      }
+      sw2_flash_counter = 0;
+      LED1 = 0;
+      //return to first state
+      sw2_state = 0;
+
+    }
+    ESOS_TASK_YIELD();
+  }
+  ESOS_TASK_END();
+}
 // UIF14 INITIALIZATION FUNCTION
 void config_esos_uiF14() {
   // setup your UI implementation
   CONFIG_RPG();
   CONFIG_LED1();
   CONFIG_LED2();
+  CONFIG_LED3();
+  CONFIG_SW1();
+  CONFIG_SW2();
 
+  sw1_state = 0;
+  _st_esos_uiF14Data.b_SW1Pressed = false;
 
   LED1 = 0;
   LED2 = 0;
+  LED3 = 0; 
 
 
   esos_RegisterTimer(dbnc_rpg, 1);
   esos_RegisterTimer(debug_rpg, 10);
+  esos_RegisterTask(switch_t1);
+  esos_RegisterTask(switch_t2);
 }
 
 
